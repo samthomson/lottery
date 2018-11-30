a4bbefae39435a6330cc926ca8dbf2c08b33e574
Sam Thomson <samt@samt.st>
2018-11-30 21:42:18 +0700

    pick winner

--- lottery.sol
+++ lottery.sol
@@ -17,4 +17,9 @@ contract Lottery {
     function random() private view returns (uint) {
         return uint(keccak256(block.difficulty, now, players));
     }
+    
+    function pickWinner() public {
+        uint winnerIndex = random() % players.length;
+        players[winnerIndex].transfer(this.balance);
+    }
 }
